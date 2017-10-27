359f423c72ef7320d70cee3f381690946c90d72a
MeryLarsen <brezo.al@gmail.com>
2017-10-27 11:43:23 +0200

    Upload complete rightDate.js exercise!

--- rightDate.js
+++ rightDate.js
@@ -18,19 +18,15 @@ var months = ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "a
 function monthNumToName(month) {
     return months[month - 1] || '';
 }
-// function monthNameToNum(monthname) {
-//     var month = months.indexOf(monthname);
-//     return month ? month + 1 : 0;
-// }
 
 var date = day + "/" + month + "/" + year;
-var date_regex = /^(0[1-9]|1[0-2])\/(0[1-9]|1\d|2\d|3[01])\/(19|20)\d{2}$/ ;
+var date_regex = /^((0?[13578]|10|12)(-|\/)(([1-9])|(0[1-9])|([12])([0-9]?)|(3[01]?))(-|\/)((19)([2-9])(\d{1})|(20)([01])(\d{1})|([8901])(\d{1}))|(0?[2469]|11)(-|\/)(([1-9])|(0[1-9])|([12])([0-9]?)|(3[0]?))(-|\/)((19)([2-9])(\d{1})|(20)([01])(\d{1})|([8901])(\d{1})))$/ ;
 
 if(!(date_regex.test(date))) {
 	console.log("El formato de fecha es incorrecto");
 } else {
 	console.log("La fecha introducida es " + date);
-	console.log("La fecha introducida es " + day + " de " + monthNumToName + " de " + year);
+	console.log("La fecha introducida es " + day + " de " + months[month - 1] + " de " + year);
 }
 
 
